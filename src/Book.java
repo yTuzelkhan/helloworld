0fd1d6cb9718a8a26e5ed02e59f9346ded231c34
Yermek Tuzelkhan <252080@astanait.edu.kz>
2025-12-10 17:21:59 +0500

    ///

---
+++ src/Book.java
@@ -0,0 +1,40 @@
+public class Book {
+    private int id;
+    private static int idGen=1;
+    private String title;
+    private String author;
+    private int year;
+    private boolean available;
+    public Book(){
+        id = idGen++;
+        available = true;
+    }
+    public Book(String title, String author, int year) {
+        this();
+        setter(title,author,year);
+    }
+    public void setter(String title, String author, int year) {
+        if(title==null||author==null|| year<1500 || year>2025){
+            throw new IllegalArgumentException("invalid title or author or the year");
+        }
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+    public void markAsBorrowed(){
+        available = false;
+    }
+    public int getId(){
+        return id;
+    }
+    public boolean available1(){
+        return available;
+    }
+    public void markAsReturned(){
+        available = true;
+    }
+    @Override
+    public String toString() {
+        return "id:" + id + "\ntitle:" + title + "\nauthor:" + author + "\nyear:" + year + "\navailable:" + available;
+    }
+}
---
+++ src/LibraryApp.java
@@ -0,0 +1,118 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class LibraryApp {
+    public ArrayList<Book> books = new ArrayList<>();
+    Scanner cin = new Scanner(System.in);
+    public void run(){
+      while(true){
+          System.out.println(" Welcome to Library App!\n" +
+                          "1. Print all books\n" +
+                          "2. Add new book\n" +
+                          "3. Search books by title\n" +
+                          "4. Borrow a book\n" +
+                          "5. Return a book\n" +
+                          "6. Delete a book by id\n" +
+                          "7. Quit "
+                  );
+          int choice = cin.nextInt();
+          switch(choice){
+              case 1:
+                  if(books==null)System.out.println("No books in the library;");
+                  else{
+                      for(int i=0;i<books.size();i++){System.out.println(books.get(i).toString());}
+                  }
+                  break;
+               case 2:
+                   addBook();
+                   break;
+              case 3:
+                  System.out.print("Enter part of the title: ");
+                  String search = cin.nextLine().toLowerCase();
+                  boolean found = false;
+
+                  for (Book b1 : books) {
+                      if (b1.toString().toLowerCase().contains(search)) {
+                          System.out.println(b1.toString());
+                          found = true;
+                      }
+                  }
+                  if (!found) {
+                      System.out.println("No matching books found.");
+                  }
+                  break;
+              case 4:
+                  System.out.print("Enter id: ");
+                  int id = cin.nextInt();
+                  for (Book b2 : books) {
+                      if (b2.getId() == id) {
+                          if (b2.available1()) {
+                              b2.markAsBorrowed();
+                              System.out.println("Book borrowed!");
+                          } else {
+                              System.out.println("Book is already borrowed.");
+                          }
+                          return;
+                      }
+                  }
+                  System.out.println("Book not found.");
+                  break;
+              case 5:
+                  System.out.print("Enter id: ");
+                  int id1 = cin.nextInt();
+                  for (Book b3 : books) {
+                      if (b3.getId() == id1) {
+                          if (!b3.available1()) {
+                              b3.markAsReturned();
+                              System.out.println("Book returned!");
+                          } else {
+                              System.out.println("This book was not borrowed.");
+                          }
+                          return;
+                      }
+                  }
+                  System.out.println("Book not found.");
+                  break;
+              case 6:
+                  System.out.print("Enter id: ");
+                  int id3 = cin.nextInt();
+
+                  for (Book b4 : books) {
+                      if (b4.getId() == id3) {
+                          books.remove(b4);
+                          System.out.println("Book deleted!");
+                          return;
+                      }
+                  }
+                  System.out.println("Book not found.");
+                  break;
+              case 7:
+                  System.out.print("GOOOOD BYEEEEE !!!! ");
+                  return ;
+          }
+      }
+    }
+    private void addBook() {
+        String title;
+        do {
+            System.out.print("Enter title: ");
+            title = cin.nextLine();
+            if (title.isEmpty()) {
+                System.out.println("Title cannot be empty. Please try again.");
+            }
+        } while (title.isEmpty());
+
+        System.out.println("Enter author: ");
+        String author = cin.nextLine();
+
+        System.out.println("Enter year: ");
+        int year = Integer.parseInt(cin.nextLine());
+
+        Book b = new Book(title, author, year);
+        books.add(b);
+
+        System.out.println("Book added!");
+    }
+    public static void main(String[] args) {
+        new LibraryApp().run();
+    }
+}
