23d28f249073a46c905b43743cc3d48b95c9fbd2
Yermek Tuzelkhan <252080@astanait.edu.kz>
2025-12-10 16:21:01 +0500

    ///

---
+++ src/Rectangle.java
@@ -0,0 +1,38 @@
+public class Rectangle {
+    private double w;
+    private double h;
+    private int id;
+    private static int idGen=1;
+    public Rectangle() {
+        w = 1.0;
+        h = 1.0;
+        this.id=idGen++;
+    }
+    public Rectangle(double w, double h) {
+        this();
+        setters(w,h);
+    }
+    void setters(double w, double h) {
+        if(w>0 && h>0){
+            this.w = w;
+            this.h = h;
+        }
+        else throw new IllegalArgumentException("Width and height must be > 0");
+    }
+    double area() {
+        return w*h;
+    }
+    double perimeter() {
+        return 2*(w+h);
+    }
+    @Override
+    public String toString() {
+        return "Rectangle id=" + id + "\n" +
+                "width=" + w + "\n" +
+                "height=" + h ;
+    }
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle(5,4);
+        System.out.println(r.toString());
+    }
+}
